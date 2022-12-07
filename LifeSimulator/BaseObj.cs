d56fca5340215997daf93b7445040cc2f6fc1ad6
Kni9t <clone111@bk.ru>
2022-12-08 00:15:31 +0300

    First create BaseObj

---
+++ LifeSimulator/BaseObj.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSimulator
+{
+    public abstract class BaseObj
+    {
+        string Name { get; set; }
+        public BaseObj(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
